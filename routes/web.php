918b525ae0b96252f086d53fc4a2c858d48f8c52
filaakbar <112999117+filaakbr@users.noreply.github.com>
2023-09-22 18:45:08 +0700

    Tugas2
    Signed-off-by: filaakbar <112999117+filaakbr@users.noreply.github.com>

--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
-Route::get('/nama', function () {
-    return ('Fila Akbar');
-});
-Route::get('/nim', function () {
-    return ('21.31.0003');
-});
+// Route::get('/nama', function () {
+//     return ('Fila Akbar');
+// });
+// Route::get('/nim', function () {
+//     return ('21.31.0003');
+// });
